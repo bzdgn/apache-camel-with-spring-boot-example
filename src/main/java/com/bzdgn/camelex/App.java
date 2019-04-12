598b85c2a21cbd124a2b3726648e25d413543f1e
Levent Divilioglu <divilioglu@gmail.com>
2019-04-12 15:41:29 +0200

    first commit

---
+++ src/main/java/com/bzdgn/camelex/App.java
@@ -0,0 +1,13 @@
+package com.bzdgn.camelex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
